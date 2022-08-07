edbf969da082035210e069bd674b1a7c3f0378f7
nonyil <igbrendow@gmail.com>
2022-08-07 13:05:05 -0300

    feat: :memo: defined entities
    challenge / correction / students / submission

--- src/domain/entities/correction.ts
+++ src/domain/entities/correction.ts
@@ -11,8 +11,13 @@ export class Correction extends Entity<CorrectionProps> {
     super(props, id);
   }
 
-  public create(props: CorrectionProps, id?: string) {
-    const correction = new Correction(props);
+  static create(props: CorrectionProps, id?: string) {
+    // if (props.grade < 0 || props.grade > 10) {
+    //   throw new Error('Grade must be between 0 and 10');
+    // }
+
+
+    const correction = new Correction(props, id);
 
     return correction;
   }
