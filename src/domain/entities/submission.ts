8da6f7e1df13e39aadec7946fc4208e3b1c20c73
nonyil <igbrendow@gmail.com>
2022-08-07 13:20:51 -0300

    feat: :fire: Create-challenger Use Case
    create-challenge-submission

--- src/domain/entities/submission.ts
+++ src/domain/entities/submission.ts
@@ -3,7 +3,7 @@ import { Entity } from "../../core/domain/Entity";
 type SubmissionProps = {
   challengeId: string;
   studentId: string;
-  createdAt: Date;
+  createdAt?: Date;
 };
 
 export class Submission extends Entity<SubmissionProps> {
@@ -11,8 +11,11 @@ export class Submission extends Entity<SubmissionProps> {
     super(props, id);
   }
 
-  public create(props: SubmissionProps, id?: string) {
-    const submission = new Submission(props, id);
+  static create(props: SubmissionProps, id?: string) {
+    const submission = new Submission({
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    }, id);
 
     return submission;
   }
